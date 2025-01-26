5ca599e30dc7db0066b50b922e725b6d4659a7c4
Missdivyaa <divyanshisharma1th@gmail.com>
2025-01-26 21:50:25 +0530

    fixed footer

--- src/components/dashboardlayout/footer.tsx
+++ src/components/dashboardlayout/footer.tsx
@@ -1,68 +1,45 @@
-import { Github, Linkedin, Instagram, Youtube,Twitter ,X} from "lucide-react";
+import Image from 'next/image';
 
 export const Footer = () => {
   const SocialIcons = () => (
-  <div className="flex gap-6">
-  <a href="#" className="text-[#454545] hover:text-gray-900 transition-colors">
-    <Github className="w-6 h-6" />
-  </a>
-  <a href="#" className="text-[#454545] hover:text-gray-900 transition-colors">
-    <Linkedin className="w-6 h-6" />
-  </a>
-  <a href="#" className="text-[#454545] hover:text-gray-900 transition-colors">
-    <Instagram className="w-6 h-6" />
-  </a>
-  <a href="#" className="text-[#454545] hover:text-gray-900 transition-colors">
-    <Youtube className="w-6 h-6" />
-  </a>
-  <a href="#" className="text-[#454545] hover:text-gray-900 transition-colors">
-    <X className="w-6 h-6" />
-  </a>
-</div>
-);
+    <div className="flex gap-6">
+      <a href="#" className="text-[#454545]">
+        <Image src="/icons/linkedin.svg" alt="Linkedin" width={24} height={24} />
+      </a>
+      <a href="#" className="text-[#454545]">
+        <Image src="/icons/insta.svg" alt="Insta" width={24} height={24} />
+      </a>
+      <a href="#" className="text-[#454545]">
+        <Image src="/icons/yt.svg" alt="Youtube" width={24} height={24} />
+      </a>
+      <a href="#" className="text-[#454545]">
+        <Image src="/icons/github.svg" alt="Github" width={24} height={24} />
+      </a>
+      <a href="#" className="text-[#454545]">
+        <Image src="/icons/x.svg" alt="X" width={24} height={24} />
+      </a>
+    </div>
+  );
 
   return (
-    <footer className="w-full h-[212px] bg-white border-t mt-auto fixed bottom-0 left-0 py-4">
-      <div className="max-w-7xl mx-auto px-4 py-8 ">
-        <div className="flex flex-col items-start justify-start gap-4">
-        <div className="flex flex-col gap-2">
-          <div className="flex items-center gap-1">
-            <img
-              src="/logo.jpeg"
-              alt="Logo"
-              className="rounded w-9 h-9"
-            />
-            <span className="font-medium font-sans text-xl text-[#434343] ">
-              Google Developer Groups OnCampus
-            </span>
-          </div>
-          <div className="flex flex-col gap-4 pl-12">
-          <p className="text-[16px] leading-[19.41px] font-sans font-normal text-[#585858] ">
-            JSS Academy of Technical Education
+    <footer className="fixed bottom-0 h-[212px] w-full border-t-4 border-t-[#DDE3FF] bg-[#F5F7FF]">
+      <div className="flex items-start justify-start gap-4 px-20 pt-[42px]">
+        <Image src="/logo.svg" alt="Logo" width={36} height={24} className="mt-[6px]" />
+        <div>
+          <p className="mb:1 h-[24px] font-[#434343] text-sm font-medium sm:mb-2 sm:text-xl">
+            Google Developer Groups OnCampus
           </p>
-          
-          </div>
-          <div className="sm:hidden ml-10 mt-6">
-              <SocialIcons />
-            </div>
-
-          <p className="text-sm text-[#454545] opacity-70 mt-4 md:hidden">
-              © 2025 GDG JSATEN
-            </p>
-          </div>
-          </div>
-          
-          <div className="flex flex-col  justify-between">
-          <div className="flex gap-[30px] justify-end ">
+          <p className="font-[#585858] text-sm font-normal md:text-base">
+            JSS Academy of Technical Education{' '}
+          </p>
+        </div>
+      </div>
+      <div className="flex h-[78px] flex-col-reverse items-start px-20 text-[#454545] sm:flex-row sm:items-center sm:justify-between sm:pb-[15.8px]">
+        <p className="text-base font-normal text-[#454545]">© 2025 GDG JSATEN</p>
+        <div className="mb-1 flex h-[24px] w-[294px] items-center gap-[30px] sm:justify-end">
           <SocialIcons />
-          </div>
-          
-          <p className="hidden  md:block opacity-70 text-[16px] leading-[19.41px] font-sans font-normal text-[#454545]">
-            © 2025 GDG JSATEN
-          </p>
         </div>
-        </div>
-
+      </div>
     </footer>
   );
 };
