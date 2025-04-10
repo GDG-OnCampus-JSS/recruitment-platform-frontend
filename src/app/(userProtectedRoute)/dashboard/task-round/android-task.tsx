14b4cc1b3500606d3cba028064390f8175edaa7a
avinashankur <avinashhankur@gmail.com>
2025-04-10 21:02:48 +0530

    Add new images and improve layout for task components

--- src/app/(userProtectedRoute)/dashboard/task-round/android-task.tsx
+++ src/app/(userProtectedRoute)/dashboard/task-round/android-task.tsx
@@ -71,8 +71,8 @@ export const AndroidTask = ({ year, onSubmit, form }: AndroidTaskProps) => {
 
       {yearTasks.map((task, index) => (
         <React.Fragment key={task.type}>
-          <div className="space-y-6">
-            <div className="flex items-center justify-between">
+          <div className="">
+            <div className="mb-5 flex items-center justify-between">
               <h3 className="text-nowrap text-[28px] font-medium">{task.title}</h3>
               <div className="h-[1px] bg-gradient-line sm:w-[981px]"></div>
             </div>
@@ -80,7 +80,7 @@ export const AndroidTask = ({ year, onSubmit, form }: AndroidTaskProps) => {
             <div className="space-y-4">
               <div className="gap-4">
                 <div className="text-[16px] font-normal leading-[25.6px] text-[#353535]">
-                  <div className="space-y-2">
+                  <div className="space-y-3">
                     {task.description.map((paragraph: string, pIndex: number) => {
                       const isHeading = !paragraph.includes('.') && paragraph.length < 50;
 
@@ -110,7 +110,7 @@ export const AndroidTask = ({ year, onSubmit, form }: AndroidTaskProps) => {
             </div>
 
             {/* Judgement Criteria and Brownie Points */}
-            <div className="flex flex-col gap-4 md:flex-row">
+            <div className="my-12 flex flex-col gap-4 md:flex-row">
               <Card className="w-full border-main">
                 <CardHeader className="pb-2">
                   <CardTitle className="text-[20px] font-medium text-[#6B83FF]">
@@ -118,7 +118,7 @@ export const AndroidTask = ({ year, onSubmit, form }: AndroidTaskProps) => {
                   </CardTitle>
                 </CardHeader>
                 <CardContent>
-                  <ul className="list-disc pl-4 text-sm text-[#353535]">
+                  <ul className="list-disc space-y-2 pl-4 text-sm text-[#353535]">
                     {task.criteria.map((criterion, cIndex) => (
                       <li key={cIndex}>{criterion}</li>
                     ))}
@@ -133,7 +133,7 @@ export const AndroidTask = ({ year, onSubmit, form }: AndroidTaskProps) => {
                   </CardTitle>
                 </CardHeader>
                 <CardContent>
-                  <ul className="list-disc pl-4 text-sm text-[#353535]">
+                  <ul className="list-disc space-y-2 pl-4 text-sm text-[#353535]">
                     {task.browniePoints.map((point, bpIndex) => (
                       <li key={bpIndex}>{point}</li>
                     ))}
