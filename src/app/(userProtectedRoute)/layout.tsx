e4a03fd188fcd6fba345150a49f1bab17bc3717b
avinashankur <avinashhankur@gmail.com>
2025-04-10 23:58:05 +0530

    Enhance user protected route layout to ensure proper loading state and user redirection

--- src/app/(userProtectedRoute)/layout.tsx
+++ src/app/(userProtectedRoute)/layout.tsx
@@ -8,27 +8,38 @@ import useUserStore from '@/stores/userStore';
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const router = useRouter();
   const user = useUserStore((state) => state.user);
+  const [storeHydrated, setStoreHydrated] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
-  // useEffect(() => {
-  //   if (!user || user === null) {
-  //     router.push('/login');
-  //   }
+  useEffect(() => {
+    const storedData = localStorage.getItem('user-storage');
+    if (storedData !== null) {
+      setStoreHydrated(true);
+    } else {
+      setStoreHydrated(true);
+    }
+  }, []);
 
-  //   setIsLoading(false);
-  // }, [user, router]);
+  useEffect(() => {
+    if (!storeHydrated) return;
 
-  // if (isLoading) {
-  //   return (
-  //     <div className="flex h-screen w-full items-center justify-center">
-  //       <Spinner className="text-theme" />
-  //     </div>
-  //   );
-  // }
+    if (!user) {
+      router.push('/login');
+      return;
+    }
 
-  // if (user) {
+    setIsLoading(false);
+  }, [user, storeHydrated, router]);
+
+  if (!storeHydrated || isLoading) {
+    return (
+      <div className="flex h-screen w-full items-center justify-center">
+        <Spinner className="text-theme" />
+      </div>
+    );
+  }
+
   return <>{children}</>;
-  // }
 };
 
 export default Layout;
