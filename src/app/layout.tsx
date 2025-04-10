92adaa08704216745755711f540ce6986a0a8297
Ramit Vishwakarma <vishwakarmaramit@gmail.com>
2025-04-10 14:56:22 +0530

    Added speed analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { type Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
@@ -38,6 +39,7 @@ export default function RootLayout({
         <Toaster />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
