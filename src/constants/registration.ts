013c2f43f0dfe7cff01ce6fe4fc085953fe0f6b7
AbhinavGupta <bengupta786@gmail.com>
2026-02-21 02:06:35 +0530

    Enable registration button

--- src/constants/registration.ts
+++ src/constants/registration.ts
@@ -1,4 +1,4 @@
-export const isRegistrationDisabled = true;
+export const isRegistrationDisabled = false;
 export const isGoogleLoginDisabled = true;
 export const userAvatarVariant = 'botttsNeutral' as const;
 
